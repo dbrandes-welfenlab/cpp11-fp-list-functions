dd38836c7be42c4936ebb5a4f8039f4baf5ef362
Daniel Brandes <dbrandes@welfenlab.de>
2021-02-27 19:34:15 +0100

    mini-fix for eigen directory move.

--- get-extern.sh
+++ get-extern.sh
@@ -16,8 +16,8 @@ tar -xvzf cpp11_range.tar.gz
 rm cpp11_range.tar.gz
 mv cpp11-range-2021.02.27 cpp11_range
 
-wget https://gitlab.com/dbrandes-welfenlab/eigen/-/archive/v2021.02.25/eigen-v2021.02.25.tar.gz
-mv eigen-v2021.02.25.tar.gz eigen.tar.gz
+wget https://gitlab.com/dbrandes-welfenlab/eigen/-/archive/v2021.02.25/eigen-v2021.02.27.tar.gz
+mv eigen-v2021.02.27.tar.gz eigen.tar.gz
 tar -xvzf eigen.tar.gz
 rm eigen.tar.gz
-mv eigen-v2021.02.25 eigen
+mv eigen-* eigen
